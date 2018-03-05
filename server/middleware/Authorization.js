ba7e9d2e5485fb38e2b67136fa7227d2b491b92f
clintfidel <clintfidel@gmail.com>
2018-03-05 11:02:53 +0100

    feature: checks that a user is successfully logged in

---
+++ server/middleware/Authorization.js
@@ -0,0 +1,48 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.load();
+const key = process.env.secretKey;
+
+/**
+   * @description - Checks if logged in user has valid AUTH token
+   *
+   * @param  {Object} req - request
+   *
+   * @param  {object} res - response
+   *
+   * @param {Object} next - Call back function
+   *
+   * @return {null} - null
+   */
+const isLoggedIn = (req, res, next) => {
+  let token;
+  const tokenAvailable = req.headers.authorization ||
+    req.headers['x-access-token'];
+  if (req.headers.authorization) {
+    [token] = req.headers.authorization.split(' ');
+  } else {
+    token = tokenAvailable;
+  }
+  if (token) {
+    jwt.verify(token, key, (error, decoded) => {
+      if (error) {
+        res.status(401)
+          .send({
+            message: 'Failed to Authenticate Token',
+            error
+          });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(401)
+      .json({
+        message: 'Access denied, Authentication token does not exist'
+      });
+  }
+};
+export default isLoggedIn;
+
