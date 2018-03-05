798959f6a65fb57e165db78eaf926bb68cd4054f
clintfidel <clintfidel@gmail.com>
2018-03-05 01:33:03 +0100

    feature: create dummy models for api endpoints

---
+++ server/dummy-models/index.js
@@ -0,0 +1,13 @@
+import UserModel from './UserModel';
+import BusinessModel from './BusinessModel';
+import CategoryModel from './CategoryModel';
+import ReviewModel from './ReviewModel';
+
+const models = {
+  UserModel,
+  BusinessModel,
+  CategoryModel,
+  ReviewModel
+};
+
+export default models;
