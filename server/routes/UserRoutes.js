51d6c527379da7b8b9b4eed33221a252bca89803
clintfidel <clintfidel@gmail.com>
2018-03-05 18:09:42 +0100

    chore: add endpoints to get all users

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -2,17 +2,23 @@ import express from 'express';
 import User from '../controllers/UserController';
 import { checkUserInput, userNameOrEmailExist } from '../middleware/validation';
 
-const { addUser, getUser, login } = User;
+const {
+  addUser, getUser, login
+} = User;
 
 const userRouter = express.Router();
 
 userRouter.route('/signup')
   .post(checkUserInput, userNameOrEmailExist, addUser);
 
+userRouter.route('/')
+  .get(getUser);
+
 userRouter.route('/login')
   .post(login);
 
-userRouter.route('/')
-  .get(getUser);
+// userRouter.route('/updateProfile/:id')
+//   .post(updateUserProfile);
+
 
 export default userRouter;
