64877d5fbc815657789abc3ab7860a1764b13d46
clintfidel <clintfidel@gmail.com>
2018-03-12 00:28:16 +0100

    database configuratio

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+    },
+  });
+  User.associate = (models) => {
+    User.hasMany(models.Business, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+    User.hasMany(models.Review, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+    User.hasMany(models.Rate, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+  };
+  return User;
+};
