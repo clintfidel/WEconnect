dec47f95c2b37c4bc4c827a1c8b726b189b3e20a
clintfidel <clintfidel@gmail.com>
2018-03-13 05:32:02 +0100

    feature: implement user login using actual database

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -49,6 +49,54 @@ class UserController {
         });
       });
   }
+
+  /**
+   * @description - logs a user in with authenticated details
+   *
+   * @param  {object} req - request object
+   *
+   * @param  {object} res - response object
+   *
+   * @return {Object} - Object containing user detail in token form
+   *
+   * Route: POST: /users/signin
+   */
+  static login(req, res) {
+    if (!req.body.username || !req.body.password) {
+      return res.status(400)
+        .json({
+          message: 'Please provide your username or password to login'
+        });
+    }
+    return User
+      .findOne({
+        where: { username: req.body.username }
+      })
+      .then((user) => {
+        if (user &&
+          bcrypt.compareSync(req.body.password, user.password)) {
+          const currentUser = omit(
+            user.dataValues,
+            ['password', 'createdAt', 'updatedAt']
+          );
+          const expireIn = { exp: Math.floor(Date.now() / 1000) + (60 * 60) };
+          const token = jwt.sign(
+            { expireIn, currentUser },
+            secret
+          );
+          return res.status(200)
+            .json({
+              message: 'Logged In Successfully',
+              token
+            });
+        }
+        return res.status(401)
+          .json({
+            message: 'Invalid Credentials.'
+          });
+      })
+      .catch(() => res.status(500).json('Internal server error'));
+  }
 }
 
 export default UserController;
