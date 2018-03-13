e3a0d0902fe08beccd83f4ccf6c0ad72b71877f9
clintfidel <clintfidel@gmail.com>
2018-03-13 05:10:09 +0100

    chore: change route declarations

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import winston from 'winston';
 import validator from 'express-validator';
 import bodyParser from 'body-parser';
-import UserRouter from './dummyServer/routes/UserRoutes';
-import BusinessRouter from './dummyServer/routes/BusinessRoutes';
+// import UserRouter from './dummyServer/routes/UserRoutes';
+// import BusinessRouter from './dummyServer/routes/BusinessRoutes';
+import UsersRouter from './server/routes/userRoutes';
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 
-app.use('/api/v1/auth', UserRouter);
-app.use('/api/v1/business', BusinessRouter);
+// app.use('/api/v1/auth', UserRouter);
+// app.use('/api/v1/business', BusinessRouter);
+app.use('/api/v1/auth', UsersRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('testing out Mock-data');
