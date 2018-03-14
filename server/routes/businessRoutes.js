a9b259f8c6600fc1085cd609230f5533d3366e0e
clintfidel <clintfidel@gmail.com>
2018-03-14 15:12:55 +0100

    chore: endpoint declaration for edit business profile

--- server/routes/businessRoutes.js
+++ server/routes/businessRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import business from '../controllers/BusinessController';
 import isLoggedIn from '../middlewares/authorization';
 import { checkBusinessInput } from '../middlewares/validation';
+import business from '../controllers/BusinessController';
 
-const { addBusiness } = business;
+const { addBusiness, updateBusiness } = business;
 const businessRouter = express.Router();
 
 businessRouter.route('/')
   .post(isLoggedIn, checkBusinessInput, addBusiness);
+
+businessRouter.route('/:businessId')
+  .put(isLoggedIn, checkBusinessInput, updateBusiness);
 
 export default businessRouter;
