5794a6d564f43a1483aeec7f6eee6b037a1fbf5c
clintfidel <clintfidel@gmail.com>
2018-03-14 09:21:36 +0100

    chore: define business model

---
+++ server/models/business.js
@@ -0,0 +1,58 @@
+module.exports = (sequelize, DataTypes) => {
+  const Business = sequelize.define('Business', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+      unique: {
+        args: true,
+        message: 'Blog title already exist '
+      }
+    },
+    details: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      required: true
+    },
+    location: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      required: true
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      required: true,
+      defaultValue: 0
+    },
+    views: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'User',
+        key: 'id',
+        as: 'userId'
+      }
+    }
+  });
+  Business.associate = (models) => {
+    Business.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
+    // Business.hasMany(models.Review, {
+    //   foreignKey: 'businessId',
+    //   onDelete: 'CASCADE',
+    //   hooks: true,
+    // });
+    // Business.hasMany(models.Rate, {
+    //   foreignKey: 'businessId'
+    // });
+  };
+  return Business;
+};
