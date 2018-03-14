b7ac2048068ad7ee2dcdb61068ced1a5dd8f8d06
clintfidel <clintfidel@gmail.com>
2018-03-14 21:00:08 +0100

    chore: endpoint declaration for user to get all businesses

--- server/routes/businessRoutes.js
+++ server/routes/businessRoutes.js
@@ -3,11 +3,14 @@ import isLoggedIn from '../middlewares/authorization';
 import { checkBusinessInput } from '../middlewares/validation';
 import business from '../controllers/BusinessController';
 
-const { addBusiness, updateBusiness, deleteBusiness } = business;
+const {
+  addBusiness, updateBusiness, deleteBusiness, getAllBusinessess
+} = business;
 const businessRouter = express.Router();
 
 businessRouter.route('/')
-  .post(isLoggedIn, checkBusinessInput, addBusiness);
+  .post(isLoggedIn, checkBusinessInput, addBusiness)
+  .get(isLoggedIn, getAllBusinessess);
 
 businessRouter.route('/:businessId')
   .put(isLoggedIn, checkBusinessInput, updateBusiness);
