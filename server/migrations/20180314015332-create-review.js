50b94521cdaae4f73f4f93308d13f42f533b478b
clintfidel <clintfidel@gmail.com>
2018-03-15 07:27:31 +0100

    chore: define migration for reviews

---
+++ server/migrations/20180314015332-create-review.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Reviews', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    userId: {
+      type: Sequelize.INTEGER
+    },
+    businessId: {
+      type: Sequelize.INTEGER
+    },
+    comments: {
+      type: Sequelize.TEXT
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Reviews')
+};
