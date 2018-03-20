2e54b224d27347d90914095d08ccc2d33afca4ec
clintfidel <clintfidel@gmail.com>
2018-03-20 15:13:42 +0100

    chore: test user authorization for all protected routes

---
+++ server/test/e-authorizationTes.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+import supertest from 'supertest';
+import app from '../../server';
+
+describe('Business: ', () => {
+  it('should fail to authenticate token', (done) => {
+    supertest(app)
+      .get('/api/v1/business')
+      .send({
+        token: '$ggggfejgfjjbvjbbvbjebf.uhvhehgvygyuf.ihfihiuhfivibviu'
+      })
+      .expect(401)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res.body.message).toBe('Failed to Authenticate Token');
+        done();
+      });
+  });
+});
