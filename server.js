31d06c19b31a2327fb9f04d6a182a9368391dd79
clintfidel <clintfidel@gmail.com>
2018-03-25 21:20:14 +0100

    chore: import webpack config in app entry point

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import winston from 'winston';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import path from 'path';
 import validator from 'express-validator';
 import bodyParser from 'body-parser';
+import webpackConfig from './webpack.config.dev';
 import UserRouter from './server/routes/userRoutes';
 import BusinessRouter from './server/routes/businessRoutes';
 import ReviewRouter from './server/routes/reviewRoutes';
 
 const app = express();
-
+if (process.env.NODE_ENV !== 'production') {
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+}
+// app.use(express.static('./client/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
@@ -21,11 +28,14 @@ app.get('/', (req, res) => {
   res.status(200).send('Welcome to WEconnect Api');
 });
 
-const port = process.env.PORT || 5500;
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/index.html'));
+});
+
+const port = process.env.PORT || 5600;
 
 app.listen(port, () => {
   winston.info(`Connected on port ${port}`);
 });
 
 export default app;
-
