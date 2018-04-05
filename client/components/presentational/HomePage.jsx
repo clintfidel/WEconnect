f6c06f427265a59d10310878397a80fd65ec6967
clintfidel <clintfidel@gmail.com>
2018-04-05 05:34:16 +0100

    chore: refactor class component to function component and navigate using Links.

--- client/components/presentational/HomePage.jsx
+++ client/components/presentational/HomePage.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class HomePage extends Component {
-  render () {
-    return (
-      <div>
-      <main className="landing-page-container">
+
+const HomePage = () => (
+  <div>
+    <main className="landing-page-container">
       <section className="our-brand">
         <nav className="nav-header">
           <div className="brand-logo">
@@ -12,14 +12,14 @@ class HomePage extends Component {
           </div>
           <ul className="user">
             <li>
-              <a href="">
+              <Link to="/signup">
                 <button type="submit">Sign Up</button>
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="">
+              <Link to="/login">
                 <button type="submit">Login</button>
-              </a>
+              </Link>
             </li>
             <li>
               <a href="">
@@ -33,54 +33,60 @@ class HomePage extends Component {
         <div className="wrap">
           <h2>Business made easy!</h2>
           <p>
-            WeConnect provides a platform that brings businesses and individuals together. This platform creates awareness for businesses
-            and gives the users the ability to write reviews about the businesses they have interacted with. This is also an
-            opportunity to get insight as to starting up a business. Explore this amazing business relationship App and get
+            WeConnect provides a platform that brings
+            businesses and individuals together.
+            This platform creates awareness for businesses
+            and gives the users the ability to write reviews
+            about the businesses they have interacted with.
+            This is also an opportunity to get insight
+            as to starting up a business.
+            Explore this amazing business relationship App and get
             experienced in the experience!
           </p>
         </div>
       </section>
       <section className="footer">
-          <div className="social">
-              <a href="#">
-                <img src="/facebook-icon.png" alt=""/>
-              </a>
-              <a href="#">
-                <img src="/instagram-icon.png" alt=""/>
-              </a>
-              <a href="#">
-                <img src="/twitter-icon.png" alt=""/>
-              </a>
-              <a href="#">
-                <img src="/google-icon.png" alt=""/>
-              </a>
-            </div>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook-icon.png" alt=""/>
+          </a>
+          <a href="#">
+            <img src="/instagram-icon.png" alt=""/>
+          </a>
+          <a href="#">
+            <img src="/twitter-icon.png" alt=""/>
+          </a>
+          <a href="#">
+            <img src="/google-icon.png" alt=""/>
+          </a>
+        </div>
         <div className="contact-me">
-            <p>
-                WEconnect is here to provide you with more business information, answer any business related question and also create an
-                effective work around for your business idea. contact us via icons.
-            </p>
+          <p>
+                WEconnect is here to provide you with more business information,
+                 answer any business related question and also create an
+                effective work around for your business idea.
+                 contact us via icons.
+          </p>
         </div>
         <div className="footer-text">
-            <p>
+          <p>
               Policies: Terms of use
-            </p>
-            <p>
+          </p>
+          <p>
               Privacy
-            </p>
-            <p>
+          </p>
+          <p>
                 Authentic Items
-            </p>
+          </p>
           <hr/>
-            <p>
-                Copyright &copy; WeConnect.herokuapp.com. All Rights reserved. Designed by clintfidel
-            </p>
-          </div>
+          <p>
+                Copyright &copy; 2018 WeConnect.herokuapp.com.
+                All Rights reserved. Designed by clintfidel
+          </p>
+        </div>
       </section>
     </main>
-      </div>
-    )
-  }
-}
+  </div>
+);
 
-export default HomePage
+export default HomePage;
