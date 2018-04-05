957488704f6883f31bb90ded490db00fccdca767
clintfidel <clintfidel@gmail.com>
2018-04-05 05:39:39 +0100

    chore: set authorization token to headers

---
+++ client/utils/authorization.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+/**
+ * @description - set token to request headers
+ *
+ * @param {string} token - Default application state
+ *
+ * @returns {void} no return or void
+ */
+export default function setAuthorizationToken(token) {
+  if (token) {
+    axios.defaults.headers.common.Authorization = `clinton ${token}`;
+  } else {
+      delete axios.defaults.headers.common.Authorization; // eslint-disable-line
+  }
+}
