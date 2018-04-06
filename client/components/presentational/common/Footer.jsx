c1ef7537dbe184b91adaf9f0957ad1d0d5d537d2
clintfidel <clintfidel@gmail.com>
2018-04-06 17:54:20 +0100

    chore: footer component

---
+++ client/components/presentational/common/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <div>
+    <footer className="push">
+      <p>
+            Policies: Terms of use | Privacy| Authentic Items Copyright ©
+            WeConnect.herokuapp.com. All Rights reserved. Designed by clintfidel
+      </p>
+    </footer>
+  </div>
+);
+
+export default Footer;
