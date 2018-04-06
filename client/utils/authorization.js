36c2736a7c995df29fc8582e333fda209f1e8685
clintfidel <clintfidel@gmail.com>
2018-04-06 18:00:41 +0100

    chore: add bearer to authorization headers

--- client/utils/authorization.js
+++ client/utils/authorization.js
@@ -8,7 +8,7 @@ import axios from 'axios';
  */
 export default function setAuthorizationToken(token) {
   if (token) {
-    axios.defaults.headers.common.Authorization = `clinton ${token}`;
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
   } else {
       delete axios.defaults.headers.common.Authorization; // eslint-disable-line
   }
