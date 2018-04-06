55778e4c7b7e6e5f99739295d0a6d668c6d95caf
clintfidel <clintfidel@gmail.com>
2018-04-06 17:41:59 +0100

    chore: set authorization token to header

--- client/index.js
+++ client/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import jwtDecode from 'jwt-decode';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import './public/styles/style.scss';
+import setAuthorizationToken from './utils/authorization';
+import { SET_CURRENT_USER } from './actions/types';
 import configureStore from './store/configureStore';
 import Routes from './routes/index';
 
 
 const store = configureStore();
+const token = localStorage.getItem('token');
+
+if (token) {
+  setAuthorizationToken(token);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    user: jwtDecode(token),
+    authenticated: true
+  });
+}
 
 render(
   <Provider store={store}>
